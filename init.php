afcdc20c8f37e658797081ab5c1adb6f322bd71d
ikAppDev <67705725+ikAppDev@users.noreply.github.com>
2020-07-10 13:08:19 -0400

    Update init.php

--- init.php
+++ init.php
@@ -1,13 +1,13 @@
 
 <?php
-	include'../function/function.php';
+	include'function/function.php';
 
 	//include'connect.php'; ki ta3amli lapage na7i l cmnt
 
-	$tpl  = '../includs/' ;//header, footer and navbar directory
-	$img  = '../layout/images/' ;//images directory
-	$css  = '../layout/css/' ;//css directory
-	$js   = '../layout/js/' ;//js directory
+	$tpl  = 'includs/' ;//header, footer and navbar directory
+	$img  = 'layout/images/' ;//images directory
+	$css  = 'layout/css/' ;//css directory
+	$js   = 'layout/js/' ;//js directory
 
 	include $tpl . 'header.php';
 	include $tpl . 'navbar.php';
